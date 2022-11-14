ca1fbd8447007d732a77dda3950dfabade6a6ffa
Samuel Martins <samukamartins007@gmail.com>
2022-11-13 23:31:32 -0300

    Implementação da camada core.

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
